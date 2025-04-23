e27a4de1ac10cea0756d9b8bf7b2365d8ed164a3
Jurrum <jbdboumans@gmail.com>
2025-04-23 12:27:03 +0200

    Notes week 1

--- Notes/Notes W1.py
+++ Notes/Notes W1.py
@@ -2,7 +2,123 @@
 Week 1 - Notes
 Intelligent Interactive Systems 
 
-----
+---- Introduction -----
+
+1. System theory and fundamentals
+    - Whatis a system?
+        - elements, interact, to create functions
+        - different types of system
+            - natural, tehcnical, cuber-physcial, social, socialtechnical, complex (systems of systems)
+            - Industry 5.0: human-centric, sustainable, resilient
+            - Industry 4.0: cyber-physical systems, IoT, AI, big data, cloud computing
+
+    -   System Anatomy
+        - Components
+        - Interactions
+        - Sturcture
+        - Boundaries
+        - Function/purpose: why does it exist
+        - Isolated System, Closed System, Open System -> in relation to outside world
+        - 
+    - System Measures
+        - Complexity -> number of elements and interactions
+        - Entropy -> Disorder or uncertainty 
+        - Emergence -> new properties or behaviours that arise from the interactions of the system's components
+        - Feedback / Strength -> Influence of outputs on future system states
+        - Delay -> time between input and output
+        - Adaptability -> Ability to adjust to change
+        - Resilience -> capacity to recover from disturbances
+        - Connectivity -> degree of interdependence among components
+        - Redundancy -> backup elements or pathways
+    
+    - System Thinking Principles
+        - Holism -> the whole is greater than the sum of its parts
+        - Interconnectedness -> all elements are connected and influence each other
+        - Feedback Loops -> reinforcing changes may grow or destory the system
+        - Emergence -> new properties or behaviours that arise from the interactions of the system's components
+        - Causality -> small changes can have large effects
+        - Dynamic Complexity -> systems are dynamic and consciously evolving
+        - System Archetypes -> identofy common patterns early
+        - Mental Models -> perceptions, beliefs,and expectations influence how a system is perceived
+        - Leverage Points -> where small changes achieve significant improvements
+
+    - System Archetypes
+        - "Fixes that fail" -> short-term solutions that create long-term problems
+            - i.e. working 10 hours instead of 8
+        - "Shifting the burden" -> relying on symptomatic solutions instead of addressing the root cause
+            - i.e. taking painkillers instead of treating the cause of the pain
+        - "Tragedy of the commons" -> pursuing individual self-interest leads to collective harm
+            - i.e. overfishing in a shared lake
+
+    - System Designer (an identity)
+        - navigate relationships
+        - impact drive - long term outcomes
+        - Facilitators of collaboration / translators acorss discplines
+        - change management / adaptive learners
+
+    - 
+
+2. Intellgient interactive systems
+
+    - What makes a system intelligent?
+        - responsiveness
+        - Context awareness
+        - Continuous improvement
+
+    - Dimensions of intelligence
+        - sensing and percepiton
+        - reasoning and decision making
+        - learning and adaptation
+        - action and interaction
+
+    - Human in the loop
+        - Human-in-the-loop (HITL) systems are designed to incorporate human feedback and decision-making 
+        into the system's processes.
+        - explicit learning / controls
+        - Different from Human ON the Loop
+        - Human in the loop: the human makes the decisions / trains the 
+
+3. IIS in Manufacturing
+
+    - Combining intelligence and interactivity
+        - Aim is to augment  human abilities not replace
+        - synergy not substitution
+
+    - Designing IIS requires
+        - Empathy
+        - TEchnical fluency
+        - System thinking
+
+
+    - chellenges in manufacturing
+        - Lean vs Agile
+        - complex, global supply chains
+        - need for customization
+        - workforce augmentation
+        - emphasis on sustainability and zero defect
+        - energy costs
+        - tariffs, geopolitics, de-globalization
+
+    -  IIS in manufacturing
+        - predictive maintenance -> reactive, planned, proactive, predictive
+        - real time process opimization
+        - human-robot collaboration
+        - AR/XR interface for training and repairs
+
+4. IIS in Healthcare
+5. IIS in Energy
+6. Course Organization
+
+- energy supply - next 1-2 years
+- Water supply in the next 3 years
+
+
+
+----- Workhshop -----
+- 
+download Factorio on Steam
+
+
 
 
 """
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # IIS-IDE
 Intelligent Interactive Systems
+
+This is the REPO for the MSC elective course Intelligent Interactive Systems
