a41f9596c11144e9030b33223ef9d786c8251d76
Joram <jbdboumans@gmail.com>
2025-05-21 11:32:30 +0200

    notes W5 added

---
+++ Notes/Notes W5.py
@@ -0,0 +1,70 @@
+"""
+Week 5 (lecture 4) - Notes
+Intelligent Interactive Systems 
+-----------------------------------------------
+Just read the final recap in the slides
+-----------------------------------------------
+
+Assignment - 
+
+Lecture notes:
+Human Computer Interaction (HCI)
+- HCI is combining 3 main areas
+    - Computer Science
+    - Cognitive Science
+    - Human Factors Enngineering
+    - HCI in NL is considered a Psychology discipline
+    - There is ALWAYS a component of Evaluation
+
+- Good HCI:
+    - improves efficienycy, safety, user trust
+   - aligining interfaces with cognitive demands
+   -  
+
+- history
+    - 1940 punchards
+    - 1960 CLI, command line interface
+    - 1970 GUI, graphical user interface
+    - 2010 NUI, natural user interface
+
+Adaptive systems
+- 'automatic adaptation to the user'
+
+
+- Industrial interfaces
+    - Clarity
+    - REpsonseives
+    - Error recovery
+
+- SCADA, Supervisory Control and Data Acquisition system
+
+- MAking something fully software based can intoruce massive potential risks
+- Hardware safety measures (like installing a fuse) must be taken into account
+
+- Affordances & Signifiers
+    - Affordance: the properties of an object that determine how it could be used
+    - Signifier: a sign that communicates the affordance of an object
+
+- Feedback loops & Perceived Control
+    - User action
+    - Feedback
+    - Modalities
+    - Trust increases, the more a user interacts with a system with the same expected outcome
+
+- Mental Models in Complex Environements
+    - what we believe will occur
+
+- visibility & mapping
+    - 
+- Consistency, Constraints & Discoverability
+
+- Cognitive Workload
+
+- Information ARchitecture
+    - F Pattern, with a lot of text
+    - Z pattern, with a lot of images
+
+- Interaction design 
+
+
+"""
